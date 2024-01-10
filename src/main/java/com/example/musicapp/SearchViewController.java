0806b7473cba700195b4a53b379a2828292c2a69
MateuszMusial <mateuszmusial95@gmail.com>
2024-01-10 20:46:57 +0100

    feat: adding search action
    Adding action for search button and adding info label

--- src/main/java/com/example/musicapp/SearchViewController.java
+++ src/main/java/com/example/musicapp/SearchViewController.java
@@ -2,15 +2,21 @@ package com.example.musicapp;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SearchViewController implements Initializable {
-
+    @FXML
+    public Text textField;
     @FXML
     private ChoiceBox<String> choiceBox;
 
@@ -24,5 +30,39 @@ public class SearchViewController implements Initializable {
         if (choiceBox != null) {
             choiceBox.setItems(FXCollections.observableArrayList(choices));
         }
+    }
+
+    public void onSearchButtonClick() throws IOException {
+        boolean isSongFound = false;
+
+        String userInput = textInput.getText();
+
+        // search song function
+
+        //  zapisz piosenke na dysku jesli znaleziona
+
+
+        if(isSongFound){
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("player-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("MusicApp");
+            stage.setScene(scene);
+            stage.show();
+        }
+        else{
+            textField.setText("Song not found!");
+        }
+    }
+
+    public void onBackButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("MusicApp");
+        stage.setScene(scene);
+        stage.show();
     }
 }
