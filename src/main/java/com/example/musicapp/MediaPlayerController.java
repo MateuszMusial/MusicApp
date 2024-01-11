5cbeb135f2b03c22c5d8805173b4a2ef9e67faf6
MateuszMusial <mateuszmusial95@gmail.com>
2024-01-11 18:11:05 +0100

    feat: adding volume bar
    Adding volume control and stop playing function

--- src/main/java/com/example/musicapp/MediaPlayerController.java
+++ src/main/java/com/example/musicapp/MediaPlayerController.java
@@ -1,9 +1,11 @@
 package com.example.musicapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -21,8 +23,15 @@ public class MediaPlayerController {
     public Label totalTimeLabel;
     @FXML
     public Label songTitleLabel;
+    @FXML
+    public Slider volumeSlider;
 
     private String fileName = "tomorrow.mp3";
+
+    public void initialize() {
+        // Set the volume slider value to the initial volume of the media player
+        volumeSlider.setValue(0.5);
+    }
 
 
     public void updateMediaInformation(String title, String artist, String totalTime) {
@@ -41,8 +50,10 @@ public class MediaPlayerController {
 
     @FXML
     protected void onBackButtonClick() throws IOException {
-        Stage currentStage = (Stage) artistLabel.getScene().getWindow();
+
+        Stage currentStage = (Stage) volumeSlider.getScene().getWindow();
         currentStage.close();
+
 
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("menu-view.fxml"));
@@ -51,13 +62,27 @@ public class MediaPlayerController {
         stage.setTitle("MusicApp");
         stage.setScene(scene);
         stage.show();
+
     }
 
     protected void playSong(String fileName){
         String path = Objects.requireNonNull(getClass().getResource(fileName)).getPath();
         Media media = new Media(new File(path).toURI().toString());
         mediaplayer = new MediaPlayer(media);
+
+        mediaplayer.setVolume(volumeSlider.getValue());
+
+
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                mediaplayer.setVolume(newValue.doubleValue()));
+
         mediaplayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaplayer.play();
+    }
+
+    public void onStopButtonClickedButtonClicked() {
+        if (mediaplayer != null) {
+            mediaplayer.stop();
+        }
     }
 }
